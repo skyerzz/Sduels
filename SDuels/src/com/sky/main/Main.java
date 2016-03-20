49614bfd3126ed440a7d02afd83aabd3cb0845c4
skyerzz <spammen67@gmail.com>
2016-03-20 21:11:53 +0100

    config + plugin added

--- SDuels/src/com/sky/main/Main.java
+++ SDuels/src/com/sky/main/Main.java
@@ -1,5 +1,8 @@
 package com.sky.main;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,13 +11,38 @@ public class Main extends JavaPlugin implements Listener{
 	@Override
 	public void onEnable() 
 	{
-		getLogger().info("Let go of your earthly tether.");
+		getLogger().info("Enabling Sduels");
 	}
 	
 	@Override	
 	public void onDisable() 
 	{
-		
+		getLogger().info("Disabling Sduels");		
+	}
+	
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		  if(cmd.getName().equalsIgnoreCase("duel"))
+		  {
+			  if(!(sender instanceof Player))
+			  {				  
+				  return true;
+			  }
+			  Player player = (Player) sender;
+			  if(args.length == 0)
+			  {
+				  sendHelpMessage(player);
+				  return true;
+			  }
+			  
+		  }
+		return true;
+	}
+	
+	public void sendHelpMessage(Player player)
+	{
+		player.sendMessage("§7Help for SDuel");
+		player.sendMessage("TODO");
 	}
 	
 }
